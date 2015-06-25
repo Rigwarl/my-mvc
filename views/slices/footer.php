4cea50c2be663c223821cf3d8e90f2ba6b330ee2
Rigwarl <Avips@mail.ru>
2015-06-25 22:57:36 +0300

    add info to footer

--- views/slices/footer.php
+++ views/slices/footer.php
@@ -1,3 +1,7 @@
-<p>it is a footer</p>
+<footer class="container">
+	<p class="pull-left">Site is under construction, please be patient</p>
+	<p class="pull-right">2015</p>
+</footer>
+
 </body>
 </html>
