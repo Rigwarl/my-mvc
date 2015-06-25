35fbb5a7b18cce507a5091d84d2fdd7f0c79e27b
Rigwarl <Avips@mail.ru>
2015-06-25 21:38:50 +0300

    remove hove/add

--- controllers/Home.php
+++ controllers/Home.php
@@ -7,12 +7,4 @@ Class Home extends Controller {
 
 		$this->view->render('home/index', $args);
 	}
-
-	public function add(){
-		$args = array(
-			'title' => 'Home/add',
-			'body' => 'hello world'
-		);
-		$this->view->render('home/add', $args);
-	}
 }
