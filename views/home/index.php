bca2d37b0967c1017e9491fb95afbabdf60fbd5e
Rigwarl <Avips@mail.ru>
2015-06-25 21:42:32 +0300

    removed all doubled tags

--- views/home/index.php
+++ views/home/index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title><?php echo $title; ?></title>
-</head>
-<body>
-	<h1><?php echo $title; ?></h1>
-	<p><?php echo $body; ?></p>
-</body>
-</html>
+
+<h1><?php echo $title; ?></h1>
+<p><?php echo $body; ?></p>
