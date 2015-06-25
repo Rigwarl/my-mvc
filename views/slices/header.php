e022936d052ac0e45b8fa5db85000c60c3b45ff3
Rigwarl <Avips@mail.ru>
2015-06-25 22:15:28 +0300

    add navbar

--- views/slices/header.php
+++ views/slices/header.php
@@ -8,8 +8,27 @@
 </head>
 <body>
 
-<header class="page-header">
-	<div class="container">
-		  <p class="h1">Rate them all!</p>
-	</div>
+<header class="container">
+	<nav class="navbar navbar-default">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="/">Brand</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		  <ul class="nav navbar-nav">
+			<li class="active"><a href="/professors">Professors</a></li>
+		  </ul>
+		  <ul class="nav navbar-nav navbar-right">
+			<li><a href="#">Login</a></li>
+			<li><a href="#">Register</a></li>
+		  </ul>
+		</div><!-- /.navbar-collapse -->
+	</nav>
 </header>
