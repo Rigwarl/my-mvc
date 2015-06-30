cff750c6b11a5765cb993387781d37ce64741527
Rigwarl <Rigwarl@Aleksandrs-iMac.Dlink>
2015-06-30 12:43:42 +0300

    fixed path to html lib

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require_once '../core/bootstrap.php';
 require_once '../core/controller.php';
 require_once '../core/model.php';
 require_once '../core/view.php';
-require_once '../core/html.php';
+require_once '../libs/html.php';
 
 $url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : 'home';
 $app = new Bootstrap($url);
