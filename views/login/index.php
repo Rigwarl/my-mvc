7d9c8648a31563f9dc06bdddb74687b3b4c33ca5
Rigwarl <Avips@mail.ru>
2015-07-02 17:36:33 +0300

    add login view

---
+++ views/login/index.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <h1>Log in</h1>
+    <form method="post" action="/login">
+      <div class="form-group">
+        <label for="login">Email address</label>
+        <input type="text" name="login" class="form-control" id="login" placeholder="login">
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-primary">Log in</button>
+    </form>
+</div>
