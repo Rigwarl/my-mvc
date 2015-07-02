90f578e2f8edb1e4935191a90686b5353f922f77
Rigwarl <Avips@mail.ru>
2015-07-02 17:37:46 +0300

    make login possible

--- controllers/login.php
+++ controllers/login.php
@@ -3,6 +3,22 @@
 Class login extends Controller{
 
 	public function index(){
-		// todo
+		// check if already logged
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			$this->auth();
+		} else {
+			$this->view->render('login/index');
+		}
+	}
+
+	private function auth(){
+		$this->users->login($_POST);
+
+		if ($_SESSION['logged']){
+			header('Location: /');
+		} else {
+			// todo error message
+			$this->view->render('login/index');
+		}
 	}
 }
--- models/users_model.php
+++ models/users_model.php
@@ -4,10 +4,20 @@ Class Users_Model extends Model{
 
 	function __construct(){
 		parent::__construct();
+		$this->table = 'users';
 		session_start();
 	}
 
-	public function login($user){
+	public function login($data){
 		// todo
+		$user = $this->get(array(
+			'login' => $data['login'],
+			'password' => $data['password']
+		))[0];
+
+		if ($user) {
+			$_SESSION['logged'] = true;
+			$_SESSION['login'] = $user['login'];
+		}
 	}
 }
