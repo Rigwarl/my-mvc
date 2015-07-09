81e1be0d6adf812d176f4f1e8babedb538c77d19
Rigwarl <Avips@mail.ru>
2015-07-09 17:00:49 +0300

    refactor auth views

--- views/auth/login.php
+++ views/auth/login.php
@@ -4,15 +4,15 @@
     <?php echo $incorrect ? "<p class='alert alert-danger'>$incorrect</p>" : ''; ?>
 
     <form method="post" action="/auth/login">
-      <div class="form-group <?php echo $login ? 'has-error' : ''?>">
-        <label for="login">
-          <?php echo $login ? "<span class='text-danger '>$login</span>" : 'Login'; ?>
+      <div class="form-group<?php echo $login ? ' has-error' : ''?>">
+        <label class="control-label" for="login">
+          <?php echo $login ?: 'Login'; ?>
         </label>
         <input type="text" name="login" class="form-control" id="login" placeholder="login">
       </div>
-      <div class="form-group <?php echo $password ? 'has-error' : ''?>">  
-        <label for="password">
-          <?php echo $password ? "<span class='text-danger '>$password</span>" : 'Password'; ?>
+      <div class="form-group<?php echo $password ? ' has-error' : ''?>">  
+        <label class="control-label" for="password">
+          <?php echo $password ?: 'Password'; ?>
         </label>
         <input type="password" name="password" class="form-control" id="password" placeholder="Password">
       </div>
--- views/auth/registration.php
+++ views/auth/registration.php
@@ -6,27 +6,27 @@
     <?php endif; ?>
 
     <form method="post" action="/auth/register" <?php echo $invalid ? 'class="has-error"' : ''?>>
-      <div class="form-group">
-        <label for="login">
-          <?php echo $login ? "<span class='text-danger '>$login</span>" : 'Login'; ?>
+      <div class="form-group<?php echo $login ? ' has-error' : ''; ?>">
+        <label class="control-label" for="login">
+          <?php echo $login ?: 'Login'; ?>
         </label>
         <input type="text" name="login" class="form-control" id="login" placeholder="Login">
       </div>
-      <div class="form-group">
-        <label for="email">
-          <?php echo $email ? "<span class='text-danger '>$email</span>" : 'Email'; ?>
+      <div class="form-group<?php echo $email ? ' has-error' : ''; ?>">
+        <label class="control-label" for="email">
+          <?php echo $email ?: 'Email'; ?>
         </label>
         <input type="text" name="email" class="form-control" id="email" placeholder="Email">
       </div>
-      <div class="form-group">
-        <label for="password">
-          <?php echo $password ? "<span class='text-danger '>$password</span>" : 'Password'; ?>
+      <div class="form-group<?php echo $password ? ' has-error' : ''; ?>">
+        <label class="control-label" for="password">
+          <?php echo $password ?: 'Password'; ?>
         </label>
         <input type="password" name="password" class="form-control" id="password" placeholder="Password">
       </div>
-      <div class="form-group">
-        <label for="password2">
-          <?php echo $password2 ? "<span class='text-danger '>$password2</span>" : 'Password again'; ?>
+      <div class="form-group<?php echo $password2 ? ' has-error' : ''; ?>">
+        <label class="control-label" for="password2">
+          <?php echo $password2 ?: 'Password again'; ?>
         </label>
         <input type="password" name="password2" class="form-control" id="password2" placeholder="Password again">
       </div>
