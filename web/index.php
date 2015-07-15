6124e0eddf42e191dc935a7dedc14f067ee55846
Rigwarl <Avips@mail.ru>
2015-07-15 18:51:59 +0300

    make conf for errors

--- web/index.php
+++ web/index.php
@@ -1,10 +1,13 @@
 <?php
 
 require_once '../config/db.php';
+require_once '../config/errors.php';
+
 require_once '../core/bootstrap.php';
 require_once '../core/controller.php';
 require_once '../core/model.php';
 require_once '../core/view.php';
+
 require_once '../libs/html.php';
 
 $url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : 'home';
