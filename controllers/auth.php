66c017014af25db78e8a03f0f121c489e5893d3e
Rigwarl <Avips@mail.ru>
2015-07-16 15:09:01 +0300

    fix notices on login page

--- controllers/auth.php
+++ controllers/auth.php
@@ -23,6 +23,7 @@ Class Auth extends Controller{
 			}
 		}
 
+		$data['title'] = 'Log in';
 		$this->view->render('auth/login', $data);
 	}
 
--- views/pages/auth/login.php
+++ views/pages/auth/login.php
@@ -1,18 +1,18 @@
 <div class="container">
     <h1>Log in</h1>
   
-    <?php echo $incorrect ? "<p class='alert alert-danger'>$incorrect</p>" : ''; ?>
+    <?php echo isset($incorrect) ? "<p class='alert alert-danger'>$incorrect</p>" : ''; ?>
 
     <form method="post" action="/auth/login">
-      <div class="form-group<?php echo $login ? ' has-error' : ''?>">
+      <div class="form-group<?php echo isset($login) ? ' has-error' : ''?>">
         <label class="control-label" for="login">
-          <?php echo $login ?: 'Login'; ?>
+          <?php echo isset($login) ? $login : 'Login'; ?>
         </label>
         <input type="text" name="login" class="form-control" id="login" placeholder="login">
       </div>
-      <div class="form-group<?php echo $password ? ' has-error' : ''?>">  
+      <div class="form-group<?php echo isset($password) ? ' has-error' : ''?>">  
         <label class="control-label" for="password">
-          <?php echo $password ?: 'Password'; ?>
+          <?php echo isset($password) ? $password : 'Password'; ?>
         </label>
         <input type="password" name="password" class="form-control" id="password" placeholder="Password">
       </div>
