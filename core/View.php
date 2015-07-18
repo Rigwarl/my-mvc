716f36830ddb15e7775dcfa9bae28cdb7ec8da7b
Rigwarl <Avips@mail.ru>
2015-07-19 02:14:02 +0300

    add is_error method to view

--- core/View.php
+++ core/View.php
@@ -7,6 +7,12 @@ Class View {
 	private $args = array();
 	// change title in all controllers
 	public $title = 'mymvc';
+	public $errors = array();
+
+	public function is_error($name, $value = true){
+		$error = isset($this->errors[$name]) ? $this->errors[$name] : false;
+		return $error == $value;
+	}
 
 	public function render($view, $args = array()) {
 		$layout = '../views/layouts/' . $this->layout . '.php';
