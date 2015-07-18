0bf40bc1413d588cd6358208f2cf61496da01b3d
Rigwarl <Avips@mail.ru>
2015-07-18 16:22:16 +0300

    fix link url and class

--- views/pages/admin/professors/index.php
+++ views/pages/admin/professors/index.php
@@ -1,12 +1,12 @@
 <div class="container">
-	<div class="clear">
+	<div class="clearfix">
 		<h1 class="pull-left">Professors</h1>
 		<a href="/admin/professors/edit/new" class="btn btn-success pull-right">Add new professor</a>
 	</div>
 	<ul>
 		<?php foreach ($professors as $professor): ?>
 			
-			<li><a href="/professors/show/<?php echo $professor['id']; ?>"><?php echo $professor['name'] . " " . $professor['patronymic'] . " " . $professor['surname'] ; ?></a></li>
+			<li><a href="/admin/professors/edit/<?php echo $professor['id']; ?>"><?php echo $professor['name'] . " " . $professor['patronymic'] . " " . $professor['surname'] ; ?></a></li>
 
 		<?php endforeach; ?>
 	</ul>
