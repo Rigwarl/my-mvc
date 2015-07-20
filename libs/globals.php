7e9f51556024deaa596efa0701554c038760bba8
Rigwarl <Avips@mail.ru>
2015-07-20 15:21:29 +0300

    wrap all in globals in obj

--- libs/globals.php
+++ libs/globals.php
@@ -1,41 +1,43 @@
 <?php
 
-function get_one_from($value, $array){
-	return isset($array[$value]) ? $array[$value] : false;
-}
+Class globals {
+	private static function get_one_from($value, $array){
+		return isset($array[$value]) ? $array[$value] : false;
+	}
 
-function get_from($value, $array){
-	if (is_array($value)) {
-		$result = array();
+	private static function get_from($value, $array){
+		if (is_array($value)) {
+			$result = array();
 
-		foreach ($value as $item) {
-			$result[$item] = get_one_from($item, $array);
+			foreach ($value as $item) {
+				$result[$item] = self::get_one_from($item, $array);
+			}
+		} else {
+			$result = self::get_one_from($value, $array);
 		}
-	} else {
-		$result = get_one_from($value, $array);
+
+		return $result;
 	}
 
-	return $result;
-}
-
-function set_array($array_old, $array_new){
-	foreach ($array_new $key => $value){
-		$array_old[$key] = $value; 
+	private static function set_array($array_old, $array_new){
+		foreach ($array_new as $key => $value){
+			$array_old[$key] = $value; 
+		}
 	}
-}
 
-function post($value){
-	return get_from($value, $_POST);
-}
+	public static function post($value){
+		return self::get_from($value, $_POST);
+	}
 
-function get($value){
-	return get_from($value, $_GET);
-}
+	public static function get($value){
+		return self::get_from($value, $_GET);
+	}
 
-function session($value){
-	return get_from($value, $_SESSION);
-}
+	public static function session($value){
+		return self::get_from($value, $_SESSION);
+	}
 
-function set_session($array){
-	return set_array($_SESSION, $array);
+	public static function set_session($array){
+		return self::set_array($_SESSION, $array);
+	}
 }
