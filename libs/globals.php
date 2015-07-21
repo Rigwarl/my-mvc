20c6a46b0a515edeec897578690f81397b0cabb5
Rigwarl <Avips@mail.ru>
2015-07-21 16:06:42 +0300

    change set_session and add unset_session

--- libs/globals.php
+++ libs/globals.php
@@ -25,6 +25,12 @@ Class globals {
 		}
 	}
 
+	private static function unset_array($array_old, $array_new){
+		foreach ($array_new as $item){
+			unset($array_old[$item]); 
+		}
+	}
+
 	public static function post($value){
 		return self::get_from($value, $_POST);
 	}
@@ -37,13 +43,22 @@ Class globals {
 		return self::get_from($value, $_SESSION);
 	}
 
-	public static function set_session($array){
-		return self::set_array($_SESSION, $array);
+	public static function set_session($value, $value_string = true){
+		if (!is_array($value)){
+			$value = array( $value => $value_string );
+		}
+		return self::set_array($_SESSION, $value);
+	}
+
+	public static function unset_session($value){
+		if (is_array($value)){
+			unset_array($_SESSION, $value);
+		} else {
+			unset($_SESSION[$value]);
+		}
 	}
 
 	public static function is_post(){
 		return $_SERVER['REQUEST_METHOD'] === 'POST';
 	}
-
-
 }
