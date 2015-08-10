dfd294b06698ac7e20e8f9be8c6624613e4251b6
Rigwarl <Avips@mail.ru>
2015-08-10 17:42:44 +0300

    add search to professors

--- controllers/professors.php
+++ controllers/professors.php
@@ -3,7 +3,12 @@
 Class Professors extends Controller{
 
 	public function index(){
-		$data = array('professors' => $this->model->getAll());
+		$data = array(
+			'name'       => '',
+			'patronymic' => '',
+			'surname'    => '',
+			'professors' => $this->model->getAll()
+		);
 
 		$this->view->title = 'All professors';
 		$this->view->render('professors/index', $data);
@@ -15,5 +20,25 @@ Class Professors extends Controller{
 
 		$this->view->title = $title = 'Professor ' . $data['name'] . ' ' . $data['patronymic'] . ' ' . $data['surname'];
 		$this->view->render('professors/show', $data);
+	}
+
+	public function find(){
+		$data = array();
+		$search = globals::post(array('surname', 'name', 'patronymic'));
+
+		if (!($search['name'] || $search['patronymic'] || $search['surname'])) {
+			//if search rules empty show all
+			$this->view->errors['empty'] = true;
+			$data['professors'] = $this->model->getAll();
+			$title = 'All professors';
+
+		} else {
+			//if not empty show relevant
+			$data['professors'] = $this->model->find($search);
+			$title = 'Search results for - ' . $search['name'] . ' ' . $search['patronymic'] . ' ' . $search['surname'];
+		}
+
+		$this->view->title = $title;
+		$this->view->render('professors/index', array_merge($search, $data));
 	}
 }
--- views/pages/professors/index.php
+++ views/pages/professors/index.php
@@ -1,12 +1,36 @@
 
 <div class="container">
-	<h1>Professors</h1>
+	<h1><?php echo $this->title; ?></h1>
 
-	<ul>
-		<?php foreach ($professors as $professor): ?>
-			
-			<li><a href="/professors/show/<?php echo $professor['id']; ?>"><?php echo $professor['name'] . " " . $professor['patronymic'] . " " . $professor['surname'] ; ?></a></li>
+	<?php if($this->is_error('empty')): ?>
+	    <p class='alert alert-danger'>Search rules must not be empty...</p>
+	<?php endif; ?>
 
-		<?php endforeach; ?>
-	</ul>
+	<form method="post" action="/professors/find" class="form-inline">
+	  <div class="form-group">
+		<label class="control-label" for="name">Name</label>
+		<input type="text" name="name" class="form-control" id="name" placeholder="Name" value="<?php echo $name; ?>">
+	  </div>
+	  <div class="form-group">  
+		<label class="control-label" for="patronymic">Patronymic</label>
+		<input type="patronymic" name="patronymic" class="form-control" id="patronymic" placeholder="Patronymic" value="<?php echo $patronymic; ?>">
+	  </div>
+	  <div class="form-group">  
+		<label class="control-label" for="surname">Surname</label>
+		<input type="surname" name="surname" class="form-control" id="surname" placeholder="Surname" value="<?php echo $surname; ?>">
+	  </div>
+	  <button type="submit" class="btn btn-primary">Search</button>
+	</form>
+
+	<?php if ($professors): ?>
+		<ul>
+			<?php foreach ($professors as $professor): ?>
+				
+				<li><a href="/professors/show/<?php echo $professor['id']; ?>"><?php echo $professor['name'] . " " . $professor['patronymic'] . " " . $professor['surname'] ; ?></a></li>
+
+			<?php endforeach; ?>
+		</ul>
+	<?php else: ?>
+		<p class="alert alert-warning">Nothing found, we are sorry...</p>
+	<?php endif; ?>
 </div>
