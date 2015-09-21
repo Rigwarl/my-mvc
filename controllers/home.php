71b4f7f00eb74b38fee3e2e1c3a46e82c7532aed
Rigwarl <Avips@mail.ru>
2015-09-21 11:50:00 +0300

    change var

--- controllers/home.php
+++ controllers/home.php
@@ -4,7 +4,6 @@ Class Home extends Controller {
 
 	public function index(){
 		$data = array(
-			'title' => 'Home/index',
 			'body' => 'hello world'
 		);
 
--- views/pages/home/index.php
+++ views/pages/home/index.php
@@ -1,7 +1,7 @@
 
 <div class="container">
 	
-	<h1><?php echo $title; ?></h1>
+	<h1><?php echo $this->title; ?></h1>
 	<p><?php echo $body; ?></p>
 	
 </div>
